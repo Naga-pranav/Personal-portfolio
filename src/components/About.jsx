50da3167c9256d5fb9971f16423280348a6a7270
myname <pranavanaga@gmail.com>
2022-12-09 23:24:17 +0530

    add-portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className="w-full b-screen
+    bg-gradient-to-b from-gray-800 to-black text-white  ">
+        <div className="max-w-screen-lg p-4 mx-auto 
+        flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4
+                border-gray-500">About</p>
+            </div>
+            <p className="text-xl mt-20">
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+               Iusto reiciendis nesciunt quod, porro quisquam pariatur saepe 
+               et impedit, assumenda recusandae dolores laborum fugit 
+               molestias a distinctio eaque? Corrupti error repellendus 
+               voluptatum ducimus laboriosam? Quisquam nemo, sint fuga 
+               nesciunt sit blanditiis amet mollitia in! Vero, error earum? 
+               Cum accusantium fuga nam!
+            </p>
+
+            <br />
+
+            <p className="text-xl">
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+               Iusto reiciendis nesciunt quod, porro quisquam pariatur saepe 
+               et impedit, assumenda recusandae dolores laborum fugit 
+               molestias a distinctio eaque? Corrupti error repellendus 
+               voluptatum ducimus laboriosam? Quisquam nemo, sint fuga 
+               nesciunt sit blanditiis amet mollitia in! Vero, error earum? 
+               Cum accusantium fuga nam!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div name="portfolio" className="bg-gradient-to-b from-black
+     to-gray-800 w-full h-screen">Portfolio</div>
+  )
+}
+
+export default Portfolio
