bd30f0ad708ecb5df549ba0c6ca9809f2f04beb1
myname <pranavanaga@gmail.com>
2022-12-09 23:39:49 +0530

    refactor-minor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
           <div className="flex flex-col justify-center h-full">
             <h2 className="text-4xl sm:text-7xl font-bold text-white">
-              I'm a BAck Stack Developer
+              I'm a Full Stack Developer
             </h2>
             <p className="text-gray-500 py-4 max-w-md">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. 
